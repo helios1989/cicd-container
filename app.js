3508f7784b42588c7b00a41a2ce02bcb5acecd32
verge <verge@LPSs-Mac-mini.local>
2020-04-05 15:48:38 +1200

    updates

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello VERGELBARIT!"));
+app.get("/", (req, res) => res.send("Hello VERGELBARIT TEST CICD!"));
 
 app.get("/health", (req, res) => {
   res.status(200);
